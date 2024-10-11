c2b123965f327cd6d564ecd32296c474b965fc74
Jeff Michaud <cometaj2@comcast.net>
2024-10-10 19:40:24 -0700

    go to an older version of numpy to continue supporting python 3.8 and 3.9

--- hcli_hai/package.py
+++ hcli_hai/package.py
@@ -3,4 +3,4 @@ dependencies = ["openai==1.7.0",
                 "tiktoken==0.5.2",
                 "anthropic==0.34.2",
                 "sumy==0.11.0",
-                "numpy==2.1.2"]
+                "numpy==1.24.4"]
