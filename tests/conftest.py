3b905fa1903d5896fe7c606cdb680163da7ecaca
Jeff Michaud <cometaj2@comcast.net>
2025-03-23 17:28:30 -0700

    additional log output for circleci failure

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,13 +31,20 @@ core.auth = False" > ./test_credentials
 
     gunicorn --workers=1 --threads=100 -b 0.0.0.0:18080 "hcli_core:connector(config_path=\\\"./test_credentials\\\",plugin_path=\\\"`hcli_hai path`\\\")" --daemon --log-file=./gunicorn.log --error-logfile=./gunicorn-error.log --capture-output
 
+
     sleep 2
 
+    curl http://127.0.0.1:18080
     huckle cli install http://127.0.0.1:18080
 
     """
-    process = subprocess.Popen(['bash', '-c', setup], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    process = subprocess.Popen(['bash', '-c', setup], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
+    result = out.decode('utf-8')
+    error = err.decode('utf-8')
+
+    print(result)
+    print(error)
 
     # Verify setup worked
     assert os.path.exists('./gunicorn-error.log'), "gunicorn-error.log not found"
